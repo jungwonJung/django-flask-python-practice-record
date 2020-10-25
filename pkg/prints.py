cbfd2ef75f14ad55f465d56a5145f3efcd53150f
jungwon jung <JUNGGanzi>
2020-10-25 16:27:23 +0900

    20201025commit

---
+++ pkg/prints.py
@@ -0,0 +1,11 @@
+def prt1():
+    print("I'm nice boy!")
+
+def prt2():
+    print("I'm good boy!")
+
+
+# 단위 실행 (독립실행)
+if __name__ == '__main__':
+    prt1()
+    prt2()
---
+++ section04-1.py
@@ -0,0 +1,11 @@
+#  데이터 타입 Variable
+
+v_str1 = "Nice man"
+v_bool = True
+v_str2 = "Good boy"
+v_float = 10.3
+v_int = 7
+v_dict = {
+    "name" : "Jung",
+    "age" : 28
+}
