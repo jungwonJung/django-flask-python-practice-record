0241fe1e2510132e55a4151916b7203ab1b9b808
JUNGganzi <wjdwjd1501@gmail.com>
2020-10-31 00:21:04 +0900

    20201031/2/commit

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy # sqlalchemy 갖고온다
+from flask import render_template  #template 연결 
+from models import db
+# from flask_sqlalchemy import SQLAlchemy # sqlalchemy 갖고온다
 
-basedir = os.path.abspath(os.path.dirname(__file__))   # 현재있는 파일의 directory name 을 절대경로로 지정해준다
-dbfile = os.path.join(basedir, 'db.sqlite')   # 현재 basedir 에 db.sqlit 라는 db 파일을 생성한다
+# basedir = os.path.abspath(os.path.dirname(__file__))   # 현재있는 파일의 directory name 을 절대경로로 지정해준다
+# dbfile = os.path.join(basedir, 'db.sqlite')   # 현재 basedir 에 db.sqlit 라는 db 파일을 생성한다
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile 
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True   # teardown 할때마다 commit 을 한다 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile 
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True   # teardown 할때마다 commit 을 한다 
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-class Test(db.Model):    # M V C
-    __tablename__ : 'test_table'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), unique=True)
-
-db.create_all()
+# db = SQLAlchemy(app)   models.py 로 이동
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('hello.html')  #hello.html 이라는 파일로 연결  templates 라는 폴더에 바로연결
+
+if __name__ == "__main__":
+    basedir = os.path.abspath(os.path.dirname(__file__))  
+    dbfile = os.path.join(basedir, 'db.sqlite')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile 
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)     # 24~26 config 설정하는 명령어가 굉장히 많지만 그걸 초기화시키는코드
+    db.app = app     # app 을 명시적으로 입력가능
+    db.create_all()
+    app.run(host='127.0.0.1', port=5000, debug=True)   # 기존의 flask 명령어 말고 파이썬명령어로 실행가능하게 개선
